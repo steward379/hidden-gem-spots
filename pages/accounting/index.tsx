bc808e8c2c8291091dd6e0da6dcc155c5feca4ed
steward379 <steward379@gmail.com>
2023-10-24 00:02:49 +0800

    Add `Entry` import

--- pages/accounting/index.tsx
+++ pages/accounting/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Form from '../../components/Form'
 import EntryList from '../../components/EntryList'
 import Link from 'next/link';
+import { Entry } from '../../types/entry';
 
 export default function Accounting() {
     const [entries, setEntries] = useState([]); 
