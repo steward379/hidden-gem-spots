baac5179bed04f059226ed7f94afbf65a2ff5cd6
steward379 <steward379@gmail.com>
2023-10-30 12:12:50 +0800

    Layout Design (tailwind) for map and inputs split

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,18 +1,40 @@
-import dynamic from 'next/dynamic';
-import React from 'react';
+// src/pages/map/index.tsx
 
-// 導入地圖組件並設置 `ssr` 為 false
+import dynamic from 'next/dynamic';
+import React, { useState } from 'react';
+
 const MapComponentWithNoSSR = dynamic(
-    () => import('../../components/MapComponent'),
+  () => import('../../components/MapComponent'),
   { ssr: false }
 );
 
 const MapDemoPage: React.FC = () => {
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  const startDrawing = () => {
+    setIsDrawing(true);
+    // ...之後的邏輯會在這裡實現
+  };
+
   return (
-    <div className="h-screen">
-      <MapComponentWithNoSSR />
+    <div className="flex flex-col md:flex-row h-screen">
+      <div className="md:w-2/3 flex justify-center items-center">
+        <MapComponentWithNoSSR />
+      </div>
+      <div className="md:w-1/3 flex flex-col">
+        {!isDrawing && (
+          <button
+            className="m-4 p-2 bg-blue-500 text-white rounded"
+            onClick={startDrawing}
+          >
+            繪製路線
+          </button>
+        )}
+        {/* 之後的輸入框和按鈕將會在這裡實現 */}
+      </div>
     </div>
   );
 };
 
 export default MapDemoPage;
+
