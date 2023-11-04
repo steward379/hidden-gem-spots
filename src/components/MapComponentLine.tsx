ba9631c6d3b4ab46374e1acbc950e62f02d62037
steward379 <steward379@gmail.com>
2023-11-04 20:35:57 +0800

    This L.type not to be default

--- src/components/MapComponentLine.tsx
+++ src/components/MapComponentLine.tsx
@@ -3,7 +3,7 @@ import L from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
-delete L.Icon.Default.prototype._getIconUrl;
+delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'images/marker-icon-2x.png',
   iconUrl: 'images/marker-icon.png',
