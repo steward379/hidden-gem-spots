587c8b340593e8a445da169166da1655803acb9e
steward379 <steward379@gmail.com>
2023-11-06 16:25:48 +0800

    Finish Demo Page - Landing Page FOR NOW - new pictures

---
+++ src/pages/home.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import LoginComponent from '../components/EmailLogComponent';
+import GoogleLogComponent from '../components/GoogleLogComponent';
+import Image from 'next/image';
+
+import React from 'react';
+// import { useLocation } from 'react-router-dom'; //Router Context but don't use it in Next.js
+import { useRouter } from 'next/router';
+
+export default function Home() {
+    const router = useRouter();
+    const message = router.query.message;
+
+    return (
+        <>
+            <div className="container max-w-xl mx-auto border p-10 mt-10 items-center justify-center">
+                <div className="flex flex-col items-center bg-gray-300 p-10 rounded-lg">
+                    <h1 className="text-4xl mb-4 text-gray-800">Hidden Gem Spots</h1>
+                    <p className="text-xl mb-6 text-gray-500">找尋你的秘密景點</p>
+                    {message && <p className="text-red-500 p-2">{message}</p>}
+                    <Link href="/map">
+                        <div className="cursor-pointer text-lg bg-gray-800 px-8 py-2 hover:bg-gray-700 rounded shadow text-white">
+                            前往你的景點地圖
+                        </div>
+                    </Link>
+                </div>
+            </div>
+            <div className='w-full text-center flex-1 md:w-1/2 mx-auto mt-16'>
+                <Image src="/images/future-02.png" alt="Scene Image" width="1000" height="1000" 
+                    objectFit='cover' />
+                <Image src="/images/future-03.png" alt="Scene Image" width="1000" height="1000" 
+                    objectFit='cover' />
+                <Image src="/images/future-01.png" alt="Scene Image" width="1000" height="1000" 
+                    objectFit='cover' />
+            </div>
+        </>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,71 @@
-// root Index.tsx
-import React from 'react';
+// pages/landing.tsx
 import Link from 'next/link';
-// import { useLocation } from 'react-router-dom'; //Router Context but don't use it in Next.js
-import { useRouter } from 'next/router';
+import React from 'react';
+import Image from 'next/image';
 
-// clerk
-import { useUser, UserButton } from "@clerk/nextjs";
+const LandingPage = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="bg-cover bg-no-repeat bg-center h-screen flex items-center justify-center" style={{ backgroundImage: "url('/images/pexels-tyler-lastovic.jpg')" }}>
+        <div className="absolute inset-0 bg-black opacity-30 mt-16"></div>
+        <div className="text-center text-white z-10">
+            <h1 className="text-6xl font-bold mb-4">Hidden Gem Spot 旅圓</h1>
+            <p className="text-xl mb-8">發現您的下一個冒險之旅。</p>
+            <Link href="/home">
+              <button className="bg-red-400 text-red text-xl font-semibold py-3 px-5 rounded hover:bg-red-500 transition duration-300">開始探索</button>
+            </Link>
+        </div>
+      </div>
 
-export default function Home() {
+      {/* Block 1 */}
+      <div className="md:flex gap-10 items-center justify-end bg-gray-100 p-16">
+        <div className="md:flex-1 md:w-1/2">
+          <div className="md:flex items-center justify-end">
+            <Image src="/images/pexels-marina-leonova.jpg" alt="Scene Image" width="1000" height="1000" />
+          </div>
+        </div>
+        <div className=" w-full md:w-1/2 text-center md:text-left p-4 text-black">
+          <h2 className="text-4xl font-bold mb-3">1️⃣ 創造你的私人景點</h2>
+          <p className="mb-5 text-2xl"> 專屬於你的故事藏寶圖，無論是約會聖地、有故事的地方，全都一目瞭然。</p>
+          <Link href="/home">
+            <button className="text-xl max-w-xs py-3 px-5 bg-red-500 text-white font-semibold rounded hover:bg-red-700 transition duration-300">馬上新增</button>
+          </Link>
+        </div>
+      </div>
 
-    const router = useRouter();
+      {/* Block 2 */}
+      <div className="flex gap-4 flex-wrap-reverse items-center justify-center bg-white p-10">
+        <div className="flex-1 md:w-1/2 text-center md:text-right p-8 text-black" >
+          <h2 className="text-4xl font-bold mb-3">2️⃣ 收藏更多景點</h2>
+          <p className="mb-5 text-2xl"> 追蹤、收藏分享他人的景點，收納到自己的地圖中。</p>
+          <Link href="/home">
+            <button className="text-xl max-w-xs py-3 px-5 bg-blue-500 text-white font-semibold rounded hover:bg-blue-700 transition duration-300">瀏覽地圖</button>
+          </Link>
+        </div>
+        <div className="flex-1 md:w-1/2" style={{ maxHeight: "600px"}}>
+          <Image src="/images/pexels-bhupendra-singh.jpg" alt="Scene Image" width="1000" height="500" 
+            objectFit='cover'
+          />
+        </div>
+      </div>
 
-    // clerk
-    const { isSignedIn, user } = useUser();
+      {/* Block 3 */}
+      <div className="md:flex gap-10 items-center justify-end bg-gray-100 p-16">
+        <div className="flex-1 md:w-1/2" style={{ maxHeight: "600px", overflow: 'hidden' }}>
+          <Image src="/images/pexels-photo-friends.jpeg" alt="Scene Image" width="1000" height="500" 
+                objectFit='cover' />
+        </div>
+        <div className=" w-full md:w-1/2 text-center md:text-left p-4 text-black">
+          <h2 className="text-4xl font-bold mb-3">1️⃣ 原來你也在！景點交友</h2>
+          <p className="mb-5 text-2xl"> 抓取附近景點，一起去有興趣或去過的景點溜達。</p>
+          <Link href="/home">
+            <button className="text-xl max-w-xs py-3 px-5 bg-red-500 text-white font-semibold rounded hover:bg-red-700 transition duration-300">看看別人都去哪</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    // firebase-auth
-    const message = router.query.message;
+export default LandingPage;
 
-    return (
-            <div className="container max-w-xl mx-auto border p-10 mt-10 items-center justify-center">
-                <div className="flex flex-col items-center bg-gray-300 p-10 rounded-lg">
-                    <h1 className="text-4xl mb-4 text-gray-800">Hidden Gem Spots</h1>
-                    <p className="text-xl mb-6 text-gray-500">找尋你的秘密景點</p>
-                    {message && <p className="text-red-500 p-2">{message}</p>}
-                    {!isSignedIn ? (
-                    // 如果用戶未登入，顯示登入或註冊的連結
-                        <Link href="/sign-in">
-                            <div className="cursor-pointer text-lg bg-gray-800 px-8 py-2 hover:bg-gray-700 rounded shadow text-white mb-4">
-                            登入 / 註冊
-                            </div>
-                        </Link>
-                        ) : (
-                        // 如果用戶已登入，顯示個人資料或登出的連結
-                        <>
-                            <UserButton afterSignOutUrl="/"/>
-                            <div className="text-black mt-2 mb-2 ">歡迎，{user.username}</div>
-                        </>
-                    )}
-                    <Link href="/map">
-                        <div className="cursor-pointer text-lg bg-gray-800 px-8 py-2 hover:bg-gray-700 rounded shadow text-white">
-                            前往你的景點地圖
-                        </div>
-                    </Link>
-                </div>
-            </div>
-    )
-}
