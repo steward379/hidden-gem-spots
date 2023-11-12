a8875ade86bdfe8402a2ed884299acb6c66c8fa9
steward379 <steward379@gmail.com>
2023-11-12 14:48:36 +0800

    Improve map function and use useContext to manage Authentication - Basic Member Page & Allow Editing - Show Attraction List in Map Page - UseContext to manage Authentication - Map function enforcement     - Navigator geolocation     - Minimap - Simplify GoogleLogComponent & EmailLogComponent

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,33 +4,46 @@ import React, { useState, useEffect } from 'react';
 import EmailLogComponent from './EmailLogComponent';
 import GoogleLogComponent from './GoogleLogComponent';  
 import { LoginMethod } from '../LoginMethod';
-
+import Image from 'next/image';
+// context
+import { useAuth } from '../context/authContext';
 // firebase
 import { User, onAuthStateChanged, Auth } from 'firebase/auth';
 import firebase from '../utils/firebase';
 
 
 const Navbar: React.FC = () => {
-  const [user, setUser] = useState<User | null>(null);
-  const [loginMethod, setLoginMethod] = useState<LoginMethod>(LoginMethod.None);
+  const { user, loading, loginMethod, setLoginMethod, logout} = useAuth();
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(firebase.auth as Auth, (currentUser,) => {
-      setUser(currentUser);
-    });
+  // useContext 已取代
+  // useEffect(() => {
+  //   const unsubscribe = onAuthStateChanged(firebase.auth as Auth, (currentUser,) => {
+  //     setUser(currentUser);
+  //   });
 
-    return () => unsubscribe();
-  }, []);
-
+  //   return () => unsubscribe();
+  // }, []);
 
   return (
-    <div className="navbar-container bg-red-400 text-white p-4">
+    <nav className="navbar-container bg-red-400 text-white p-4">
       <div className="flex items-center space-x-4">
-      <Link href="/">
-        <h1 className="text-2xl font-bold mb-2 cursor-pointer">Hidden Gem Spots</h1>
-      </Link>
-
-      {loginMethod === LoginMethod.None && (
+        <Link href="/">
+          <h1 className="text-2xl font-bold mb-2 cursor-pointer">Hidden Gem Spots</h1>
+        </Link>
+        {user && (
+          <>
+            <Image src={user.avatar} alt="User avatar" className="rounded-full w-10 h-10" width="50" height="50" />
+            <span>你好，{user.name || '會員'}</span>
+            <Link href={`/member/${user.uid}`}>
+              <div>會員中心</div>
+            </Link>
+            <Link href={`/map`}>
+              <div>我的地圖</div>
+            </Link>
+            <button onClick={logout}>登出</button>
+          </>
+        )} 
+        {/* {loginMethod === LoginMethod.None && (
           <>
             <EmailLogComponent user={user} setUser={setUser} setLoginMethod={setLoginMethod}  LoginMethod={LoginMethod} />
             <GoogleLogComponent user={user} setUser={setUser} setLoginMethod={setLoginMethod} LoginMethod={LoginMethod}  />
@@ -41,10 +54,15 @@ const Navbar: React.FC = () => {
         )}
         {loginMethod === LoginMethod.Google && (
           <GoogleLogComponent user={user} setUser={setUser} setLoginMethod={setLoginMethod}  LoginMethod={LoginMethod}  />
+        )} */}
+        {!user && (
+          <>
+            <EmailLogComponent  />
+            <GoogleLogComponent  />
+          </>
         )}
-
       </div>
-    </div>
+    </nav>
   );
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,47 +1,51 @@
-import '../styles/globals.css';
+// _app.tsx
 
+import { AuthProvider } from "../context/authContext";
+// css
+import '../styles/globals.css';
 //map
 import 'leaflet/dist/leaflet.css';
 import dynamic from 'next/dynamic';
 
 import { AppProps } from 'next/app';
-import { useEffect } from 'react';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import firebaseServices from '../utils/firebase'; 
+const { db, auth, storage } = firebaseServices; 
 import Navbar  from '@/src/components/Navbar';
+import { getDoc, doc, setDoc} from 'firebase/firestore';
 
 
-const MyApp  = ({ Component, pageProps}: AppProps ) => {
+const MyApp = ({ Component, pageProps}: AppProps ) => {
+  // const [userId, setUserId] = useState(null);
 
-  const router = useRouter();
-  const { asPath } = router;
+  // const router = useRouter();
+  // const { asPath } = router;
 
-  useEffect(() => {
+  // useEffect(() => {
 
-    const auth = getAuth();
+  //   const auth = getAuth();
 
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (!user) {
+  //   const unsubscribe = onAuthStateChanged(auth, async (user) => {
+  //       if (asPath.startsWith("/accounting") || asPath.startsWith("/map")) {
+  //         console.log('No user login');
+  //         router.push({
+  //           pathname: "/",
+  //           query: { message: "請先登入" },
+  //         });
+  //       }
 
-        if(asPath.startsWith("/accounting") || asPath.startsWith("/map")){
-          console.log('No user login');
+  //   });
 
-          router.push({
-            pathname: "/",
-            query: { message: "請先登入" },
-          })
-        }
-      }
-    });
-
-    return () => unsubscribe();
-  }, [ asPath, router]);
+  //   return () => unsubscribe();
+  // }, [ asPath, router]);
 
   return (
-    <>
+    <AuthProvider>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </AuthProvider>
   );
 }
 
