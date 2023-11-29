c5fe1c3912d01e348dc00e70f55a5b3437a475ed
steward379 <steward379@gmail.com>
2023-11-29 08:40:39 +0800

    [feat] get ready for web notifications by adding self-defined hook

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import { useAuth } from '../context/authContext';
+import { askNotificationPermission } from '../utils/notification';
+import { useAuthListeners } from '../hooks/useAuthListeners';
 
 const Layout = ({ children }) => {
+  const { user, loaded } = useAuth();
+  
+  // useAuthListeners();
+  
+  React.useEffect(() => {
+    askNotificationPermission();
+
+  }, [user, loaded]);
+
   return (
     <>
       <Navbar />
