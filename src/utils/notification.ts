891a401659d818b1f25b454e65f2f7a3d9ca9dce
steward379 <steward379@gmail.com>
2023-11-30 10:39:41 +0800

    [fix] notification error handling

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -1,10 +1,16 @@
-// notifications.ts
+// utils/notifications.ts
 export async function askNotificationPermission() {
-    // 申請通知權限
-    return Notification.requestPermission(); 
-}
-
-//細部功能
+  if (!("Notification" in window)) {
+    console.log("這個瀏覽器不支援桌面通知");
+    return
+  } else if (Notification.permission !== "denied") {
+    return Notification.requestPermission().then(permission => {
+      if (permission === "granted") {
+        console.log("通知權限被授予");
+      }
+    });
+  }
+};
 
 export function sendNewFollowerNotification(name) {
   console.log('嘗試發送通知：', name);
@@ -14,13 +20,13 @@ export function sendNewFollowerNotification(name) {
     return;
   }
 
-  if (Notification.permission === "granted") {
+  if (Notification?.permission === "granted") {
     console.log(`有權限，新追蹤者 ${name} 關注你了`);
     const notification = new Notification("新追蹤者", {
       body: `${name} 關注你了`
     });
-  } else if (Notification.permission !== "denied") {
-    Notification.requestPermission().then((permission) => {
+  } else if (Notification?.permission !== "denied") {
+    Notification?.requestPermission().then((permission) => {
       console.log(`無權限，新追蹤者 ${name} 關注你了`);
       if (permission === "granted") {
         console.log(`又有權限，新追蹤者 ${name} 關注你了`);
@@ -42,13 +48,13 @@ export function sendNewMapPublishedNotification(map, name) {
     return;
   }
 
-  if (Notification.permission === "granted") {
+  if (Notification?.permission === "granted") {
     console.log(`有權限，${name} 發佈了新地圖 ${map}`);
     const notification = new Notification("新地圖", {
       body: `${name} 發佈了新地圖 ${map}`
     });
-  } else if (Notification.permission !== "denied") {
-    Notification.requestPermission().then((permission) => {
+  } else if (Notification?.permission !== "denied") {
+    Notification?.requestPermission().then((permission) => {
       console.log(`無權限`);
       if (permission === "granted") {
         console.log(`剛才給了權限，${name} 發佈了新地圖 ${map}`);
