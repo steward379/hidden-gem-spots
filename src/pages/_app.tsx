7cbfee51e27ca0ae222c903e972d28df15061565
steward379 <steward379@gmail.com>
2023-11-30 22:25:58 +0800

    [feat] Last Update context Provider for notifications

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
-import { AuthProvider } from "../context/authContext";
+// /pages/_app.tsx
+import { AuthProvider } from "../context/AuthContext";
 import '../styles/globals.css';
 import 'leaflet/dist/leaflet.css';
 import { AppProps } from 'next/app';
 import Layout from '../layouts/layout';
+import { MapNotificationProvider } from '../context/MapNotificationContext';
+import { LastUpdateProvider } from '../context/LastUpdateContext';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <AuthProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <LastUpdateProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </LastUpdateProvider>
     </AuthProvider>
   );
 }
