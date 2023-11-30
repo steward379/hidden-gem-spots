711b7316e7ed25b73df680143781f1fae81cdaaf
steward379 <steward379@gmail.com>
2023-11-30 23:18:00 +0800

    [feat] make the navbar responsive and build a menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,62 +1,88 @@
-// Navbar.tsx
 import Link from 'next/link';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import EmailLogComponent from './EmailLogComponent';
 import GoogleLogComponent from './GoogleLogComponent';  
-import { LoginMethod } from '../LoginMethod';
+import { useAuth } from '../context/AuthContext';
 import Image from 'next/image';
-// context
-import { useAuth } from '../context/authContext';
-// firebase
-import { User, onAuthStateChanged, Auth } from 'firebase/auth';
-import firebase from '../utils/firebase';
-
 
 const Navbar: React.FC = () => {
-  const { user, loading, loginMethod, setLoginMethod, logout} = useAuth();
+  const { user, logout } = useAuth();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  // useContext 已取代
-  // useEffect(() => {
-  //   const unsubscribe = onAuthStateChanged(firebase.auth as Auth, (currentUser,) => {
-  //     setUser(currentUser);
-  //   });
+  const menuRef = useRef(null);
+  const [menuHeight, setMenuHeight] = useState(0);
 
-  //   return () => unsubscribe();
-  // }, []);
+  useEffect(() => {
+    if (menuRef.current) {
+      setMenuHeight(menuRef.current.scrollHeight);
+    }
+  }, [isMenuOpen]);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <nav className="navbar-container fixed top-0 left-0 right-0 bg-black-200/80 text-black p-4 z-50 backdrop-blur-md">
-      <div className="flex items-center space-x-4">
-        <Link href="/">
-          <div className="flex items-end text-2xl font-bold mb-2 cursor-pointer">
-            <div>
+    <>
+      <nav className="fixed top-0 left-0 right-0 bg-stone-300 bg-opacity-30 bg-black-200/80 text-black p-4 z-50 backdrop-blur-md">
+        <div className="flex items-center justify-between">
+          <Link href="/">
+            <div className="flex items-center cursor-pointer">
               <Image src="/images/ballon.png" alt="logo" width="50" height="50" />
+              <span className="lg:text-2xl font-bold ml-2 text-md sm:block">Hidden Gem</span>
             </div>
-            <h1 className="flex text-2xl font-bold mb-2 cursor-pointer">Hidden Gem</h1>
+          </Link>
+
+          <button title="toggle-menu" onClick={toggleMenu} className="sm:hidden">
+            <i className="fas fa-bars text-xl"></i>
+          </button>
+        {/* {isMenuOpen && ( */}
+          <div className={`absolute top-full left-0 right-0  bg-opacity-80 backdrop-blur-lg p-4 sm:relative sm:bg-transparent sm:top-auto sm:p-0 transition-all duration-300 ease-in-out ${isMenuOpen ? 'block' : 'hidden'} sm:flex`}>
+            <div className="flex flex-col sm:flex-row items-center sm:items-stretch">
+              {user ? (
+                <>
+                  <Link href={`/member/${user.uid}`}>
+                    <div className="flex items-center cursor-pointer">
+                      <div className="rounded-full bg-sky-300 mix-blend-color-burn">
+                        <Image src={user.avatar || '/images/marker-icon.png'} alt="User avatar" width="30" height="30" className="rounded-full object-contain"/>
+                      </div>
+                      <div className="ml-3 text-sm sm:block">你好，{user.name || '會員'}</div>
+                    </div>
+                  </Link>
+                  <div className={`flex-grow mt-4 sm:mt-0 ${isMenuOpen ? 'flex' : 'hidden'} sm:flex flex-col sm:flex-row`}>
+                    <Link href="/home">
+                      <div className="sm:ml-4">探索</div>
+                    </Link>
+                    <Link href={`/user-maps/${user.uid}`}>
+                      <div className="mt-4 sm:mt-0 sm:ml-4">我的地圖</div>
+                    </Link>
+                    <Link href="/map">
+                      <div className="mt-4 sm:mt-0 sm:ml-4">管理景點</div>
+                    </Link>
+                    <button title="logout" onClick={logout} className="mt-4 sm:mt-0 sm:ml-4">
+                      <i className="fas fa-arrow-right-from-bracket"></i>
+                    </button>
+                  </div>
+                </>
+              ) : (
+                <div className={`mt-4 sm:mt-0 ${isMenuOpen ? 'flex' : 'hidden'} sm:flex flex-col sm:flex-row`}>
+                  <EmailLogComponent />
+                  <GoogleLogComponent />
+                </div>
+              )}
+            </div>
+            
           </div>
-        </Link>
-        {user && (
-          <>
-            <Image src={user.avatar} alt="User avatar" className="rounded-full w-10 h-10" width="50" height="50" />
-            <span>你好，{user.name || '會員'}</span>
-            <Link href={`/member/${user.uid}`}>
-              <div>會員中心</div>
-            </Link>
-            <Link href={`/user-maps/${user.uid}`}>
-              <div>我的地圖</div>
-            </Link>
-            <button onClick={logout}>登出</button>
-          </>
-        )} 
-        {!user && (
-          <>
-            <EmailLogComponent  />
-            <GoogleLogComponent  />
-          </>
-        )}
-      </div>
-    </nav>
+          {/* )} */}
+        </div>
+      </nav>
+      {isMenuOpen && <div className={` fixed transition-all inset-0 mt-20 ${ user ? "h-60" :"h-2"}  bg-white bg-opacity-50 backdrop-blur-md sm:hidden`} 
+                    onClick={() => setIsMenuOpen(false)}></div>}
+    </>
   );
 };
 
 export default Navbar;
+
+
+// border-lime-500 border border-dashed p-1 pl-3 pr-3 rounded-full 
