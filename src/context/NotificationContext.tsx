f19871be7600167f332e0a0c09879154171a962e
steward379 <steward379@gmail.com>
2023-11-30 22:25:05 +0800

    [feat] notification context for notifications

---
+++ src/context/NotificationContext.tsx
@@ -0,0 +1,17 @@
+// src/context/NotificationContext.js
+import React, { createContext, useContext, useState } from 'react';
+
+const NotificationContext = createContext();
+
+export const NotificationProvider = ({ children }) => {
+  const [lastFollowers, setLastFollowers] = useState([]);
+  const [lastMaps, setLastMaps] = useState({});
+
+  return (
+    <NotificationContext.Provider value={{ lastFollowers, setLastFollowers, lastMaps, setLastMaps }}>
+      {children}
+    </NotificationContext.Provider>
+  );
+};
+
+export const useNotificationContext = () => useContext(NotificationContext);
