5c2a603d44f40033da7a60ec59e75276b5310a92
steward379 <steward379@gmail.com>
2023-12-03 19:05:04 +0800

    [chore] waiting to be updated in footer ```

---
+++ src/components/footer/Tribute.tsx
@@ -0,0 +1,9 @@
+
+{/* <a href="https://iconscout.com/3d-illustrations/short" 
+class="text-underline font-size-sm" target="_blank">Short Hair Man With Bucket Hat</a> 
+by <a href="https://iconscout.com/contributors/moehzackbean" 
+class="text-underline font-size-sm">Big Marsȟa</a> 
+on <a href="https://iconscout.com" class="text-underline font-size-sm">IconScout</a> */}
+
+// 3d animation tribute
+
