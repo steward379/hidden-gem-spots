98b61b606ca056aaf595516dfc35d0dd10842705
steward379 <steward379@gmail.com>
2023-12-03 19:02:06 +0800

    [style] break-word

--- src/components/DropzoneImage.tsx
+++ src/components/DropzoneImage.tsx
@@ -11,7 +11,8 @@ const DropzoneImage = ({ onFileUploaded }) => {
 // Multiple Files
 
   return (
-    <div {...getRootProps()} className="mb-5 mt-5 flex justify-center items-center border-2 border-dashed border-gray-300 rounded-lg h-32 w-32 cursor-pointer hover:border-gray-500">
+    <div {...getRootProps()} className="mb-5 mt-5 flex justify-center items-center border-2 border-dashed
+                            border-gray-300 rounded-lg h-32 w-32 cursor-pointer hover:border-gray-500">
       <input {...getInputProps()} />
       <svg className="w-6 h-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4"></path>
