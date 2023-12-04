a00f6ec86e4865c5248ee2e5d468244957556482
steward379 <steward379@gmail.com>
2023-12-05 04:20:00 +0800

    [fix] 🔍 use try/catch to find unknown errors & turn off checking

--- src/hooks/useAuthListeners.ts
+++ src/hooks/useAuthListeners.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState, useCallback } from 'react';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '@/src/context/AuthContext'
 import firebaseServices from '../utils/firebase';
 import {
   doc, getDoc, collection, query, where, getDocs, serverTimestamp, updateDoc
@@ -12,71 +12,78 @@ const useAuthListeners = (handleNewNotification: (string)=>{} = null, setCheckFo
 
   const checkForNewFollowers = useCallback(async () => {
     console.log('Checking for new followers...');
-    if (!user) return;
 
-    const userDoc = await getDoc(doc(db, 'users', user.uid));
-    const userData = userDoc.data();
+    try {
+      if (!user || !navigator.onLine) return;
 
-    const lastFollowers = userData?.lastFollowers || [];
-    const currentFollowers = userData?.followers || [];
-    const newFollowers = currentFollowers.filter(followerId => !lastFollowers.includes(followerId));
+      const userDoc = await getDoc(doc(db, 'users', user.uid));
+      const userData = userDoc.data();
 
-    for (const followerId of newFollowers) {
-      const followerDoc = await getDoc(doc(db, 'users', followerId));
-      if (followerDoc.exists()) {
-        const followerData = followerDoc.data();
-        console.log('New follower:', followerData.name);
-        handleNewNotification && handleNewNotification(`${followerData.name} 來了`);
-        sendNewFollowerNotification(followerData.name);
+      const lastFollowers = userData?.lastFollowers || [];
+      const currentFollowers = userData?.followers || [];
+      const newFollowers = currentFollowers.filter(followerId => !lastFollowers.includes(followerId));
+
+      for (const followerId of newFollowers) {
+        const followerDoc = await getDoc(doc(db, 'users', followerId));
+        if (followerDoc.exists()) {
+          const followerData = followerDoc.data();
+          console.log('New follower:', followerData.name);
+          handleNewNotification && handleNewNotification(`${followerData.name} 來了`);
+          sendNewFollowerNotification(followerData.name);
+        }
       }
+      // Update last followers in the database
+      await updateDoc(doc(db, 'users', user.uid), {
+        lastFollowers: currentFollowers
+      });
+    } catch (error) {
+      console.error(error);
     }
-    // Update last followers in the database
-    await updateDoc(doc(db, 'users', user.uid), {
-      lastFollowers: currentFollowers
-    });
   }, [user, db, handleNewNotification]);
 
   const checkForNewMaps = useCallback(async () => {
-    if (!user) return;
-  
-    const userDocRef = doc(db, 'users', user.uid);
-    const userDoc = await getDoc(userDocRef);
-    const userData = userDoc.data();
-  
-    let notifiedMaps = userData.notifiedMaps || [];
-  
-    for (const followedUserId of user.following) {
-      const mapsRef = collection(db, 'publishedMaps', followedUserId, 'maps');
-      const mapsSnapshot = await getDocs(mapsRef);
-  
-      mapsSnapshot.forEach((doc) => {
-        const mapData = doc.data();
-        const mapId = doc.id;
-  
-        if (!notifiedMaps.includes(mapId)) {
-          handleNewNotification && handleNewNotification(`${mapData.title} 是 ${followedUserId} 寫的`);
-          sendNewMapPublishedNotification(mapData.title, followedUserId);
-          notifiedMaps.push(mapId);
-        }
+    try {
+      if (!user || !navigator.onLine) return;
+    
+      const userDocRef = doc(db, 'users', user.uid);
+      const userDoc = await getDoc(userDocRef);
+      const userData = userDoc.data();
+    
+      let notifiedMaps = userData.notifiedMaps || [];
+    
+      for (const followedUserId of user.following) {
+        const mapsRef = collection(db, 'publishedMaps', followedUserId, 'maps');
+        const mapsSnapshot = await getDocs(mapsRef);
+    
+        mapsSnapshot.forEach((doc) => {
+          const mapData = doc.data();
+          const mapId = doc.id;
+    
+          if (!notifiedMaps.includes(mapId)) {
+            handleNewNotification && handleNewNotification(`${mapData.title} 是 ${followedUserId} 寫的`);
+            sendNewMapPublishedNotification(mapData.title, followedUserId);
+            notifiedMaps.push(mapId);
+          }
+        });
+      }
+    
+      await updateDoc(userDocRef, {
+        notifiedMaps: notifiedMaps
       });
+    } catch (error) {
+      console.error(error);
     }
-  
-    await updateDoc(userDocRef, {
-      notifiedMaps: notifiedMaps
-    });
   }, [user, db, handleNewNotification]);
-
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       checkForNewFollowers();
       checkForNewMaps();
-    }, 10 * 60 * 1000); // Check every 10 minutes
+    }, 10 * 60 * 1000); 
 
-    checkForNewFollowers();
-    checkForNewMaps();
+    // checkForNewFollowers();
+    // checkForNewMaps();
     
-    // Check immediately on mount
     setCheckForNewFollowers && setCheckForNewFollowers(() => checkForNewFollowers);
     setCheckForNewMaps && setCheckForNewMaps(() => checkForNewMaps);
 
@@ -85,7 +92,7 @@ const useAuthListeners = (handleNewNotification: (string)=>{} = null, setCheckFo
     };
   }, [checkForNewFollowers, checkForNewMaps, setCheckForNewFollowers, setCheckForNewMaps]);
 
-  return null; // No need to return anything
+  return null;
 };
 
 export default useAuthListeners;
