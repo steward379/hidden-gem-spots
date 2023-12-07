4e6162bc1b4563e2f5f9f56e63032edee1f837b0
steward379 <steward379@gmail.com>
2023-12-07 21:32:47 +0800

    [chore] 🧹 update next.config.js ```

--- next.config.js
+++ next.config.js
@@ -5,4 +5,23 @@ const nextConfig = {
       },
 }
 
+// const path = require('path');
+
+// module.exports = {
+//   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+//     // 解決 Web3.js 的窗口對象 (window object) 問題（如果有的話）
+//     if (!isServer) {
+//       config.resolve.fallback = {
+//         fs: false,
+//         net: false,
+//         tls: false,
+//       };
+//     }
+
+//     // 在這裡添加更多自定義 webpack 配置
+
+//     return config;
+//   },
+// };
+
 module.exports = nextConfig
