4464ac31fe00f28abc5e01942a8476350357d94c
steward379 <steward379@gmail.com>
2023-12-08 03:46:13 +0800

    [feat] use AlertModal Component & link to nft Mint Page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,27 +1,34 @@
-import Link from 'next/link';
-import LoginComponent from '../components/EmailLogComponent';
-import GoogleLogComponent from '../components/GoogleLogComponent';
-import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
-// import { useLocation } from 'react-router-dom'; //Router Context but don't use it in Next.js
 import { useRouter } from 'next/router';
+import Link from 'next/link';
+import Image from 'next/image';
+
 import { useMapNotification } from '../context/MapNotificationContext';
-import { collection, collectionGroup, query, onSnapshot, getDocs } from "firebase/firestore";
+import { collectionGroup, query, onSnapshot, getDocs, orderBy } from "firebase/firestore";
+
 import firebaseServices from '../utils/firebase';
 const { db } = firebaseServices;
+
 import { useAuth } from '../context/AuthContext';
+
 import GlobeComponent from '../components/animation/GlobeComponent';
+import AlertModal from '../components/AlertModal';
+import MapCard from '../components/MapCard';
+import RainbowButtonModule from '@/src/styles/rainbowButton.module.css'
+
+// import { formatDate } from '../helpers/formDate';
 
 const ITEMS_PER_PAGE = 4; 
 
 export default function Home() {
     const router = useRouter();
-    const message = router.query.message || '';
+    const routerMessage = router.query.message || '';
 
     const { user } = useAuth();
 
     const [isAlertOpen, setIsAlertOpen] = useState(false);
     const [alertMessage, setAlertMessage] = useState('錯誤');
+
     const showAlert = (message) => {
         setAlertMessage(message);
         setIsAlertOpen(true);
@@ -35,11 +42,12 @@ export default function Home() {
     
     // switch map data
     const [unsubscribeFunction, setUnsubscribeFunction] = useState(null);
+
     useEffect(() => {
         let unsubscribe;
 
         if (currentTab === 'latest') {
-            const mapsQuery = query(collectionGroup(db, "maps"));
+            const mapsQuery = query(collectionGroup(db, "maps")); //orderBy 只能排 timestamp
             unsubscribe = onSnapshot(mapsQuery, (querySnapshot) => {
                 const maps = querySnapshot.docs.map(doc => ({
                     ...doc.data(),
@@ -47,7 +55,14 @@ export default function Home() {
                     userId: doc.ref.parent.parent.id // 獲取用戶 ID
                 }));
                 setMaps(maps);
-                console.log("lastest maps");
+                console.log("latest maps");
+
+                // 將 ISO 字串格式的 publishDate 轉換為 Date 物件，然後根據日期進行排序
+                // @ts-ignore
+                maps.sort((a, b) => new Date(b.publishDate).getTime() - new Date(a.publishDate).getTime());
+
+                setMaps(maps);
+                console.log("latest maps");
             });
         } else if (currentTab === 'popular') {
             const fetchMaps = async () => {
@@ -65,13 +80,12 @@ export default function Home() {
                 const sortedMaps = fetchedMaps.sort((a, b) => b.popularity - a.popularity).slice(0, 100);
                 setMaps(sortedMaps);
             };
+
             fetchMaps();
         }
 
-        // 保存取消訂閱函數
         setUnsubscribeFunction(() => unsubscribe);
       
-        // 清理函數
         return () => {
           if (unsubscribe) {
             unsubscribe();
@@ -104,10 +118,11 @@ export default function Home() {
     }, [currentTab]);
 
     useEffect(() => {
-        if(router.query.message) {
-            showAlert('請填寫標題');
+        if(routerMessage !== '') {
+            showAlert('請先登入');
+            router.push('/home');
         }
-    }, [message, alertMessage, isAlertOpen, router.query.message]);
+    }, [alertMessage, isAlertOpen, routerMessage, router]);
 
     const [isVisible, setIsVisible] = useState(true);
 
@@ -120,9 +135,8 @@ export default function Home() {
     return (
         <div className={`bg-gradient-to-r from-sky-500 to-cyan-300`}>
             <div className={`custom-transition-container overflow-hidden relative mx-auto mb-0 flex justify-center items-center  
-                ${!isVisible && 'custom-hidden '}`}
-                
-                style= {{ backgroundImage: 'url(/images/night.jpg)', backgroundSize: 'cover'}}
+                ${!isVisible && 'custom-hidden '}  bg-night-image`}
+                // style= {{ backgroundImage: 'url(/images/night.jpg)', backgroundSize: 'cover'}}
                 >
                 {/* <div className=" overflow-hidden "> */}
                 <div className="hard-light globe-container max-w-lg max-h-lg overflow-hidden">
@@ -133,10 +147,9 @@ export default function Home() {
                     <div className="flex flex-col items-center bg-gray-300 rounded-full p-10 backdrop-blur-sm bg-opacity-30 hover:bg-opacity-60">
                         <Image src="/images/ballon.png" alt="Scene Image" width="300" height="300" style={{ objectFit: 'cover', mixBlendMode: 'color-burn' }} />
                         <h1 className="text-4xl mb-4 text-gray-800">Hidden Gem Spots</h1>
-                        <p className="text-xl mb-6 text-gray-500">找尋你的秘密景點</p>
-                                {message && <p className="text-red-500 p-2">{message}</p>}
+                        <p className="text-xl mb-6 text-black">找尋你的秘密景點</p>
                         <Link href="/map">
-                            <div className="cursor-pointer text-lg  px-8 py-2 hover:bg-sky-700 rounded-3xl shadow transition-btn text-white">
+                            <div className="cursor-pointer text-lg  px-8 py-2 bg-blue-400 hover:bg-sky-700 rounded-3xl shadow transition-btn text-white">
                                 前往你的景點地圖
                             </div>
                         </Link>
@@ -148,6 +161,20 @@ export default function Home() {
                     <div className="mb-5 cursor-pointer text-center border-dotted border border:black rounded-full p-4 pt-9 pb-9" 
                         onClick={handleClick}> {isVisible? '隱藏地球' : '顯示地球' } 
                     </div>
+                          <div className="mb-10 flex justify-center ">
+                    <button className={`${RainbowButtonModule.rainbowButton} bg-red-500  text-white font-bold py-2 px-4 rounded hover:bg-blue-600 my-2`}
+                                    style={{
+                                        // @ts-ignore
+                                        '--button-width': '200px',
+                                        '--button-height': '50px',
+                                        '--button-border-radius': '100px'
+                                        }}> 
+                        <Link href="/mintNFT"
+                            className="bg-red-500 rounded-full text-white font-bold py-2 px-10 rounded hover:bg-blue-600 my-2">
+                            首發紀念 NFT
+                        </Link>
+                    </button>
+                </div>
                 </div>
                 <div className="flex mb-4">
                     <button 
@@ -164,14 +191,14 @@ export default function Home() {
                 {currentTab === "popular" && (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mb-10">
                     {paginatedMaps.map(map => (
-                        <MapCard key={map.id} map={map} userId={user?.uid} />
+                        <MapCard key={map.id} map={map} userId={map.userId} />
                     ))}
                 </div>
                 )}
                 {currentTab === "latest" && (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mb-10">
                     {paginatedMaps.map(map => (
-                        <MapCard key={map.id} map={map} userId={user?.uid} />
+                        <MapCard key={map.id} map={map} userId={map.userId} />
                     ))}
                 </div>
                 )}
@@ -192,6 +219,7 @@ export default function Home() {
                 </div>
             </div>
             <div className='w-full text-center flex-1 md:w-1/2 mx-auto mt-16'>
+          
                 <Image src="/images/future-02.png" alt="Scene Image" width="1000" height="1000" 
                     style={{ objectFit: 'cover' }} />
                 <Image src="/images/future-03.png" alt="Scene Image" width="1000" height="1000" 
@@ -203,42 +231,3 @@ export default function Home() {
         </div>
     )
 }
-
-
-const AlertModal = ({ isOpen, onClose, message }) => {
-    if (!isOpen) return null;
-  
-    return (
-      <div className="fixed inset-0 bg-black bg-opacity-30 z-50 flex justify-center items-center" >
-        <div className="bg-white p-6 rounded-lg shadow-xl z-10">
-          <p className="text-black">{message}</p>
-          <button onClick={onClose} className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
-            確定
-          </button>
-        </div>
-      </div>
-    );
-  };
-
-  const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    return `${date.toLocaleDateString()} ${date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}`;
-  };
-
-  const MapCard = ({ map, userId }) => {
-    return (
-      <div className="border bg-cover bg-center hover:bg-yellow-500 transition-btn rounded-3xl bg-purple-400" 
-           style={{ backgroundImage: `url('${map.coverImage}')`, height: '300px', backgroundSize: 'cover', backgroundPosition: 'center' }} >
-  
-          <div className="relative p-4 w-full h-full bg-gradient-to-t from-transparent to-blue-600 opacity-100 rounded-3xl 
-                        hover:bg-yellow-400 hover:bg-opacity-50">
-            <Link href={`/publishedMaps/${userId}/maps/${map.id}`}>
-              <h2 className="text-2xl font-semibold text-amber-400">{map.title}</h2>
-              <p className=" text-amber-200 font-bold">{formatDate(map.publishDate)}</p>
-              <div className="h-full w-full"></div>
-            </Link>
-          </div>
-      </div>
-    );
-  };
-  
