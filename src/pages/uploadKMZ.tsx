d167fc592beeeb5b1d862f9bddd914a39b096cb1
steward379 <steward379@gmail.com>
2023-12-08 03:42:16 +0800

    [feat] 🌟 uploadKMZ sample page

--- src/pages/uploadKMZ.tsx
+++ src/pages/uploadKMZ.tsx
@@ -57,7 +57,6 @@ const UploadKMZ = () => {
   //     coordinates: placemark.Point[0].coordinates[0].trim()
   //   }));
   // };
-
   const extractPlaces = (kmlData) => {
     const folders = kmlData?.kml?.Document[0]?.Folder || [];
     let allPlacemarks = [];
@@ -65,12 +64,35 @@ const UploadKMZ = () => {
     folders.forEach(folder => {
       const placemarks = folder?.Placemark || [];
       const extractedPlacemarks = placemarks.map(placemark => {
+        // 切割坐標字串
+        const coordinatesStr = placemark.Point && placemark.Point[0].coordinates[0].trim();
+        console.log('coordinatesStr', coordinatesStr);
+        const coordinates = coordinatesStr ? coordinatesStr.split(',') : [25.03, 121.56];
+        if (coordinates.length < 2) {
+          return null; // 或者設置一個預設值
+        }
+
+        let warningTag ='';
+        if (coordinates[0] === 25.03 && coordinates[1] === 121.56) {
+          warningTag = '缺乏經緯度';
+        }
+
+        if (coordinates.length < 2) {
+          return null; // 或者設置一個預設值
+        }
+  
         return {
           name: placemark.name[0],
           description: placemark.description ? placemark.description[0] : '無描述',
-          coordinates: placemark.Point && placemark.Point[0].coordinates[0].trim()
+          tags: warningTag || [], 
+          category: 'others', 
+          coordinates: {
+            lat: parseFloat(coordinates[1]),
+            lng: parseFloat(coordinates[0]) 
+          }
         };
-      });
+      }).filter(p => p !== null); // 過濾掉無效的結果
+  
       allPlacemarks = [...allPlacemarks, ...extractedPlacemarks];
     });
   
@@ -79,19 +101,42 @@ const UploadKMZ = () => {
   
 
   return (
-    <div>
-      <input title="kmz-upload" type="file" id="kmz-upload" accept=".kmz" onChange={handleFileChange} />
-      <button onClick={handleFileUpload}>上傳 KMZ 檔案</button>
-      {/* <pre>{JSON.stringify(kmlData, null, 2)}</pre> */}
-      <ul>
-        {places.map((place, index) => (
-          <li key={index}>
-            <h3>{place.name}</h3>
-            <p>{place.description}</p>
-            <p>坐標：{place.coordinates}</p>
-          </li>
-        ))}
-      </ul>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <div className="p-6 bg-white shadow-md rounded-md">
+        <h2 className="text-lg font-semibold text-gray-700 mb-2">上傳 KMZ/KML 檔案</h2>
+        <input 
+          title="kmz-upload"
+          type="file"
+          id="kmz-upload"
+          accept=".kmz,.kml"
+          onChange={handleFileChange}
+          
+          className="mb-3 block w-full text-sm text-gray-600 
+                      file:mr-4 file:py-2 file:px-4
+                      file:rounded-full file:border-0
+                      file:text-sm file:font-semibold
+                      file:bg-blue-50 file:text-blue-700
+                      hover:file:bg-blue-100"
+        />
+        <button 
+          onClick={handleFileUpload}
+          disabled={!file} // 如果沒有選擇檔案，則按鈕不可用
+          className={`px-4 py-2 rounded text-white font-bold mt-2 
+                      ${file ? 'bg-blue-500 hover:bg-blue-600' : 'bg-gray-400 cursor-not-allowed'}`} // 根據檔案是否選擇改變樣式
+        >
+          上傳
+        </button>
+        <ul className="mt-4">
+          {places.map((place, index) => (
+            <li key={index} className="mt-2 border-b border-gray-200 pb-2">
+              <h3 className="font-semibold text-gray-800">{place.name}</h3>
+              <p className="text-gray-600">{place.description}</p>
+                <p className="text-sm text-gray-500">標籤：{place?.tags}</p>
+              <p className="text-sm text-gray-500">坐標：{place.coordinates.lat}, {place.coordinates.lng}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
