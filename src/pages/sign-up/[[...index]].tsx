b5160cd706da3cc33c80bcbe750f3343537464e9
steward379 <steward379@gmail.com>
2023-12-10 02:02:57 +0800

    [style] hide footer

--- src/pages/sign-up/[[...index]].tsx
+++ src/pages/sign-up/[[...index]].tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
  
 export default function Page() {
   return ( 
-  <div className="flex justify-center align-center mt-28">
+  <div className="flex justify-center align-center mt-28  h-screen-without-navbar">
     <SignUp />
   </div>
   )
