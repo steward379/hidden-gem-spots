82a78f230c0a95d49bae899295a8de24c83c9555
steward379 <steward379@gmail.com>
2024-01-22 21:04:27 +0800

    [fix] 🧐 Change the logout destination to index ("/") ```

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -282,7 +282,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
       setShowLoginAlert(false);
       setHasNavigated(true); 
     
-      router.push('/home'); 
+      router.push('/'); 
     } catch (error) {
       console.error("登出失敗", error);
     }
